41947049de7757dd4279dd21e09ec60f6f99f7f6
Margox <margox@foxmail.com>
2018-03-16 16:26:24 +0800

    v1.8.0

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import "babel-polyfill"
 import './style.scss'
 import 'braft-editor/dist/braft.css'
 import React from 'react'
@@ -13,47 +14,8 @@ class Demo extends React.Component {
   render() {
 
     const editorProps = {
-      placeholder: 'Hello World!',
-      onHTMLChange: this.handleHTMLChange,
-      viewWrapper: '.demo',
-      contentFormat: 'html',
-      initialContent: '',
-      extendControls: [
-        {
-          type: 'split',
-        },
-        {
-          type: 'button',
-          text: '预览',
-          className: 'preview-button',
-          onClick: () => {
-            window.open().document.write(this.state.htmlContent)
-          }
-        }, {
-          type: 'dropdown',
-          text: <span>下拉菜单</span>,
-          component: <h1 style={{width: 200, color: '#ffffff', padding: 10, margin: 0}}>Hello World!</h1>
-        }, {
-          type: 'modal',
-          text: <span style={{paddingRight: 10,paddingLeft: 10}}>弹窗</span>,
-          className: 'modal-button',
-          modal: {
-            title: '这是一个弹窗',
-            showClose: true,
-            showCancel: true,
-            showConfirm: true,
-            confirmable: true,
-            onConfirm: () => console.log(1),
-            onCancel: () => console.log(2),
-            onClose: () => console.log(3),
-            children: (
-              <div style={{width: 480, height: 320, padding: 30}}>
-                <span>Hello World！</span>
-              </div>
-            )
-          }
-        }
-      ]
+      onChange: console.log,
+      onHTMLChange: console.log
     }
 
     return (
